bd2370d308444219b0553de0fec83fd37894e148
Klim Tsoutsman <klim@tsoutsman.com>
2024-09-26 10:45:59 +1000

    Update Android preference directory
    It is now set to `getDir("shared_prefs")` because that is where
    `SharedPreferences` files are stored.
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- src/android.rs
+++ src/android.rs
@@ -3,7 +3,7 @@ extern crate dirs_sys;
 use std::{env, path::PathBuf};
 
 use jni::{
-    objects::{JObject, JString},
+    objects::{JObject, JString, JValue, JValueGen},
     JNIEnv,
 };
 use BaseDirs;
@@ -20,13 +20,29 @@ pub fn user_dirs() -> Option<UserDirs> {
 
 pub fn project_dirs_from_path(project_path: PathBuf) -> Option<ProjectDirs> {
     robius_android_env::with_activity(|env, context| {
-        let cache_dir = get_path_from_fn(env, context, "getCacheDir")?;
-        let config_dir = get_path_from_fn(env, context, "getFilesDir")?;
+        const GET_DIR_SIG: &str = "(Ljava/lang/String;I)Ljava/io/File;";
+        const GET_X_DIR_SIG: &str = "()Ljava/io/File;";
+
+        let cache_dir = get_path_from_fn(env, context, "getCacheDir", GET_X_DIR_SIG, &[])?;
+
+        let config_dir = get_path_from_fn(env, context, "getFilesDir", GET_X_DIR_SIG, &[])?;
         let config_local_dir = config_dir.clone();
         let data_dir = config_dir.clone();
         let data_local_dir = config_dir.clone();
-        // TODO
-        let preference_dir = config_dir.clone();
+
+        const MODE_PRIVATE: i32 = 0;
+        let preference_dir = get_path_from_fn(
+            env,
+            context,
+            "getDir",
+            GET_DIR_SIG,
+            &[
+                JValueGen::Object(&env.new_string("shared_prefs").ok()?.into()),
+                // We only need to call `getAbsolutePath` on the file object so
+                // opening in private mode is sufficient.
+                JValueGen::Int(MODE_PRIVATE),
+            ],
+        )?;
 
         Some(ProjectDirs {
             project_path: project_path.clone(),
@@ -43,9 +59,15 @@ pub fn project_dirs_from_path(project_path: PathBuf) -> Option<ProjectDirs> {
     .flatten()
 }
 
-fn get_path_from_fn(env: &mut JNIEnv, context: &JObject, fn_name: &'static str) -> Option<PathBuf> {
+fn get_path_from_fn(
+    env: &mut JNIEnv,
+    context: &JObject,
+    fn_name: &str,
+    signature: &str,
+    args: &[JValue],
+) -> Option<PathBuf> {
     let file = env
-        .call_method(context, fn_name, "()Ljava/io/File;", &[])
+        .call_method(context, fn_name, signature, args)
         .ok()?
         .l()
         .ok()?;
